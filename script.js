f65aa7b1154274aa602f1dbd6b2166d129b8f313
Carlos <carlsdias.dev@gmail.com>
2023-01-07 22:14:53 -0300

    Adição da funcionalidade de seleção em todas as funções

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 function selecionaPrato (pratoSelecionado) {
-    const pratoAnterior = document.querySelector('.selecionado');
+    const pratoAnterior = document.querySelector('.container-pratos .selecionado');
 
     if(pratoAnterior !== null) {
         pratoAnterior.classList.remove('selecionado');
-        pratoAnterior.querySelector('.checkmark').classList.add('hidden');
+        pratoAnterior.querySelector('.container-pratos .checkmark').classList.add('hidden');
     }
 
-    const prato = document.querySelector(pratoSelecionado);
-    prato.classList.add('selecionado');
-    prato.querySelector('.hidden').classList.remove('hidden');
+
+    pratoSelecionado.classList.add('selecionado');
+    pratoSelecionado.querySelector('.hidden').classList.remove('hidden');
 }
 
+
+function selecionaBebida(bebidaSelecionada) {
+    const bebidaAnterior = document.querySelector('.container-bebidas .selecionado');
+
+    if(bebidaAnterior !== null) {
+        bebidaAnterior.classList.remove('selecionado');
+        bebidaAnterior.querySelector('.container-bebidas .checkmark').classList.add('hidden');
+}
+
+    bebidaSelecionada.classList.add('selecionado');
+    bebidaSelecionada.querySelector('.hidden').classList.remove('hidden')
+
+}
+
+function selecionaSobremesa(sobremesaSelecionada) {
+    const sobremesaAnterior = document.querySelector('.container-sobremesas .selecionado');
+
+    if(sobremesaAnterior !== null) {
+        sobremesaAnterior.classList.remove('selecionado');
+        sobremesaAnterior.querySelector('.container-sobremesas .checkmark').classList.add('hidden');
+}
+
+    sobremesaSelecionada.classList.add('selecionado');
+    sobremesaSelecionada.querySelector('.hidden').classList.remove('hidden')
+
+}
