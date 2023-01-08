d3b04c4f121116dd941b2632cba4848a13509913
Carlos <carlsdias.dev@gmail.com>
2023-01-08 00:17:06 -0300

    Adicionando botão de fechar o pedido

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let precoPrato;
+let nomePrato;
+
+let precoBebida;
+let nomeBebida;
+
+let precoSobremesa;
+let nomeSobremesa;
+
 function selecionaPrato (pratoSelecionado) {
     const pratoAnterior = document.querySelector('.container-pratos .selecionado');
 
@@ -9,6 +18,13 @@ function selecionaPrato (pratoSelecionado) {
 
     pratoSelecionado.classList.add('selecionado');
     pratoSelecionado.querySelector('.hidden').classList.remove('hidden');
+
+    nomePrato = pratoSelecionado.querySelector('h3').innerHTML;
+    console.log(nomePrato);
+    precoPrato = pratoSelecionado.querySelector('b').innerHTML;
+    console.log(precoPrato)
+
+    verificaPedidos();
 }
 
 
@@ -23,6 +39,12 @@ function selecionaBebida(bebidaSelecionada) {
     bebidaSelecionada.classList.add('selecionado');
     bebidaSelecionada.querySelector('.hidden').classList.remove('hidden')
 
+    nomeBebida = bebidaSelecionada.querySelector('h3').innerHTML;
+    console.log(nomeBebida);
+    precoBebida = bebidaSelecionada.querySelector('b').innerHTML;
+    console.log(precoBebida)
+
+    verificaPedidos();
 }
 
 function selecionaSobremesa(sobremesaSelecionada) {
@@ -36,4 +58,23 @@ function selecionaSobremesa(sobremesaSelecionada) {
     sobremesaSelecionada.classList.add('selecionado');
     sobremesaSelecionada.querySelector('.hidden').classList.remove('hidden')
 
+
+    nomeSobremesa = sobremesaSelecionada.querySelector('h3').innerHTML;
+    console.log(nomeSobremesa);
+    precoSobremesa = sobremesaSelecionada.querySelector('b').innerHTML;
+    console.log(precoSobremesa)
+
+    verificaPedidos();
+}
+
+function verificaPedidos() {
+    if (nomeBebida !== undefined){
+        if (nomePrato !== undefined){
+            if (nomeSobremesa !== undefined){
+                console.log('verificado');
+                const botaoFecharPedido = document.querySelector('button').classList.add('botão-selecionado');
+                document.querySelector('button p').innerHTML = 'Fechar Pedido';
+            }
+        }      
+    }
 }
